2d7128cfa9e195e5456f58af3866468842654795
Lucas Cavalcante <cavalcantelucasx@gmail.com>
2021-06-20 19:02:06 -0300

    chore: :wrench: configuring babel plugin root import

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,27 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'babel-plugin-root-import',
+        {
+          rootPathPrefix: '~',
+          rootPathSuffix: 'src'
+        }
+      ]
+    ],
+    env: {
+      production: {
+        plugins: [
+          [
+            'babel-plugin-root-import',
+            {
+              rootPathPrefix: '~',
+              rootPathSuffix: 'src'
+            }
+          ]
+        ]
+      }
+    }
   };
 };
