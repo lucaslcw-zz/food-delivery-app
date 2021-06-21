9b9ad55eb9244ec7e5b7dc683c98bbc255dbb111
Lucas Cavalcante <cavalcantelucasx@gmail.com>
2021-06-20 21:38:09 -0300

    feat: :sparkles: adding redux provider

--- App.tsx
+++ App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+
+import store from '~/store';
 
 import Routes from '~/routes';
 
 const App = () => (
-  <Routes />
+  <Provider store={store}>
+    <Routes />
+  </Provider>
 );
 
 export default App;
