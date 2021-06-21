6805110374f7bdca0e1fb196ce19c7c67d937f7e
Lucas Cavalcante <cavalcantelucasx@gmail.com>
2021-06-20 23:50:48 -0300

    feat(redux): :sparkles: creating menu actions

---
+++ src/store/actions/Menu.ts
@@ -0,0 +1,14 @@
+export const setCategories = (payload: any) => ({
+  type: 'SET_CATEGORIES',
+  payload,
+});
+
+export const setCategory = (payload: any) => ({
+  type: 'SET_CATEGORY',
+  payload,
+});
+
+export const setProducts = (payload: any) => ({
+  type: 'SET_PRODUCTS',
+  payload,
+});
