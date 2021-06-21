0d5f412f1e7b7828a3d8f69cea5d44297343b64b
Lucas Cavalcante <cavalcantelucasx@gmail.com>
2021-06-20 21:41:30 -0300

    feat(services): :sparkles: creating a function to get user session

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,12 +1,12 @@
 import { auth } from '~/config/Firebase';
 
-export const signIn = (email: string, password: string) => new Promise((resolve, reject) => {
+export const signIn = async (email: string, password: string) => new Promise((resolve, reject) => {
   auth.signInWithEmailAndPassword(email, password)
     .then((userInformation) => resolve(userInformation))
     .catch(() => reject());
 });
 
-export const signUp = (name: string, email: string, password: string) => new Promise<void>((resolve, reject) => {
+export const signUp = async (name: string, email: string, password: string) => new Promise<void>((resolve, reject) => {
   auth.createUserWithEmailAndPassword(email, password)
     .then(({ user }) => {
       user?.updateProfile({ displayName: name })
@@ -14,4 +14,13 @@ export const signUp = (name: string, email: string, password: string) => new Pro
         .catch(() => reject());
     })
     .catch(() => reject());
+});
+
+export const getSession = async () => new Promise((resolve) => {
+  auth.onAuthStateChanged((user) => {
+    if (user) {
+      return resolve({ uid: user.uid, name: user.displayName, email: user.email });
+    }
+    resolve(null);
+  });
 });
