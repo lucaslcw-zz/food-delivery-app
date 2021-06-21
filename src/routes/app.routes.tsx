a450a8e6ddb63016329ec93abfc28fa4596feab6
Lucas Cavalcante <cavalcantelucasx@gmail.com>
2021-06-20 22:24:53 -0300

    style(tab bottom): :art: adjusting tab bottom

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,16 +1,36 @@
-import 'react-native-gesture-handler';
 import React from 'react';
+import { Ionicons, Entypo } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-import Home from '~/screens/Home';
+import Menu from '~/screens/Menu';
 
 const Tab = createBottomTabNavigator();
 
 const AppRoutes = () => (
-  <Tab.Navigator>
+  <Tab.Navigator initialRouteName="Menu">
     <Tab.Screen
-      name="Home"
-      component={Home}
+      name="Menu"
+      component={Menu}
+      options={{
+        title: 'Cardápio',
+        tabBarIcon: ({ color, size }: any) => <Ionicons name="fast-food" size={size} color={color} />,
+      }}
+    />
+    <Tab.Screen
+      name="Ordered"
+      component={Menu}
+      options={{
+        title: 'Pedidos',
+        tabBarIcon: ({ color, size }: any) => <Entypo name="list" size={size} color={color} />,
+      }}
+    />
+    <Tab.Screen
+      name="Settings"
+      component={Menu}
+      options={{
+        title: 'Ajustes',
+        tabBarIcon: ({ color, size }: any) => <Ionicons name="ios-settings" size={size} color={color} />,
+      }}
     />
   </Tab.Navigator>
 );
