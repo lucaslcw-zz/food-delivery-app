65f8c35500e4fc295b5a8f7c6c107674f1cce2a8
Lucas Cavalcante <cavalcantelucasx@gmail.com>
2021-06-20 23:51:24 -0300

    feat(redux): :zap: adjusting root reducer

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 
 import Authentication from '~/store/reducers/Authentication';
+import Menu from '~/store/reducers/Menu';
 
 const RootReducer = combineReducers({
   Authentication,
+  Menu,
 });
 
 export default RootReducer;
