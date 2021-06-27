dd4b0509424cedffc2a28c97c41b21af88416e32
Lucas Cavalcante <cavalcantelucasx@gmail.com>
2021-06-27 12:01:43 -0300

    feat(config): :sparkles: firebase configs

---
+++ src/config/Firebase.ts
@@ -0,0 +1,17 @@
+import {
+  API_KEY,
+  AUTH_DOMAIN,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID,
+  APP_ID,
+} from 'react-native-dotenv';
+
+export default {
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
+};
