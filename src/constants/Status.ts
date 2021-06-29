86011f10c62fc9979f81b158252650e8eb2e60eb
Lucas Cavalcante <cavalcantelucasx@gmail.com>
2021-06-29 14:10:58 -0300

    feat(constants): :sparkles: creating status constants

---
+++ src/constants/Status.ts
@@ -0,0 +1,5 @@
+export const WAITING_CONFIRMATION = 'waiting confirmation';
+export const ORDER_PREPARED = 'order prepared';
+export const OUT_FOR_DELIVERY = 'out for delivery';
+export const ORDER_DELIVERY = 'order delivered';
+export const ORDER_CANCELED = 'order canceled';
